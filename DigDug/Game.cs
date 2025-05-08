d2becdc414f936b3db4e04c6c05511005fa5c5f3
Henno Théo <94365544+NazioLT@users.noreply.github.com>
2025-05-08 09:00:26 +0200

    :sparkles: Add Window

---
+++ DigDug/Game.cs
@@ -0,0 +1,26 @@
+﻿using Raylib_cs;
+
+namespace DigDug;
+
+public class Game
+{
+    public void StartGame()
+    {
+        
+    }
+
+    public void UpdateGame()
+    {
+        
+    }
+
+    public void RecordDrawing()
+    {
+        Raylib.DrawRectangle(0,0,50,50,Color.White);
+    }
+
+    public void EndGame()
+    {
+        
+    }
+}
--- DigDug/Program.cs
+++ DigDug/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DigDug;
+using Raylib_cs;
+
+Raylib.InitWindow(840,480, "Dig Dug");
+Raylib.SetTargetFPS(60);
+
+Game game = new Game();
+game.StartGame();
+
+while (!Raylib.WindowShouldClose())
+{
+    game.UpdateGame();
+    
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.Black);
+    
+    game.RecordDrawing();
+    
+    Raylib.EndDrawing();
+}
+
+game.EndGame();
+Raylib.CloseWindow();
